2331ac973c5237d7ddd48caac27acce2e169c12f
rootreboot <58434887+RootReboot@users.noreply.github.com>
2020-01-29 18:08:41 +0000

    Chnage user entity model

--- server/src/entity/User.ts
+++ server/src/entity/User.ts
@@ -1,18 +1,29 @@
-import {Entity, PrimaryGeneratedColumn, Column} from "typeorm";
-
+import {
+  Entity,
+  PrimaryColumn,
+  Column,
+  BeforeInsert,
+  BaseEntity
+} from "typeorm";
+import * as uuidv4 from "uuid/v4";
 @Entity()
-export class User {
+export class User extends BaseEntity {
+  //Doens't do a sequencial increment of ids,
+  // so the user doens't get to the have the info of how much user he have, etc, etc
+  @PrimaryColumn("uuid")
+  id: string;
 
-    @PrimaryGeneratedColumn()
-    id: number;
+  @Column("varchar", { length: 255 })
+  email: string;
 
-    @Column()
-    firstName: string;
+  @Column("text")
+  password: string;
 
-    @Column()
-    lastName: string;
+  @Column()
+  age: number;
 
-    @Column()
-    age: number;
-
+  @BeforeInsert()
+  addId() {
+    this.id = uuidv4();
+  }
 }
